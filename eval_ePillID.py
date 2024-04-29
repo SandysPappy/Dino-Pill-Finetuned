64d6ecec7185b4df02d08ffc3bf43d2c3d0cc9dd
course.cap6412 Student 5 <kevinqaz45@gmail.com>
2024-04-29 19:12:00 -0400

    upload eval_ePillID.py

--- eval_ePillID.py
+++ eval_ePillID.py
@@ -1,6 +1,6 @@
-from utils import utils
-from utils.DinoModel import DinoModel, dino_args
-from utils.utils import NpEncoder
+from myutils import utils
+from myutils.DinoModel import DinoModel, dino_args
+from myutils.utils import NpEncoder
 import torch
 import argparse
 import time 
@@ -18,8 +18,11 @@ from tqdm import tqdm
 from numpy import dot
 from numpy.linalg import norm
 import torch.nn.functional as F
-from utils import metrics
-
+from myutils import metrics
+from torch.utils.data import DataLoader
+from dataset_builders_2 import get_epill_dataset
+from dino.vision_transformer import vit_small
+from empty_model import MLP
 def initDinoV1Model(model_to_load, FLAGS, checkpoint_key="teacher", use_back_bone_only=False):
     dino_args.pretrained_weights = model_to_load
     dino_args.output_dir = FLAGS.log_dir
@@ -53,11 +56,11 @@ if __name__=="__main__":
                         help='type of mode train or test')
     parser.add_argument('--dino_base_model_weights',
                         type=str,
-                        default="./dino/pretrained/dino_vitbase8_pretrain_full_checkpoint.pth",
+                        default="./dino/pretrained/dino_deitsmall8_pretrain_full_checkpoint.pth",
                         help='dino based model weights')
     parser.add_argument('--dino_custom_model_weights',
                         type=str,
-                        default="./weights/dinoxray/checkpoint.pth",
+                        default="./dinov1_train_result_new/checkpoint.pth",
                         help='dino based model weights')
     parser.add_argument('--search_gallery',
                         type=str,
@@ -65,14 +68,14 @@ if __name__=="__main__":
                         help='dataset in which images will be searched')
     parser.add_argument('--topK',
                         type=int,
-                        default=5,
+                        default=50,
                         help='Top-k paramter, defaults to 5')
     parser.add_argument('--seed', 
                         default=0, 
                         type=int, 
                         help='Random seed.')
     parser.add_argument('--num_workers', 
-                        default=4, 
+                        default=1, 
                         type=int, 
                         help='Number of data loading workers per GPU.')
     parser.add_argument("--dist_url", 
@@ -95,8 +98,9 @@ if __name__=="__main__":
     SEED_FOR_RANDOM_SPLIT = 43
 
 
-    dinov1_model = initDinoV1Model(model_to_load=FLAGS.dino_base_model_weights,FLAGS=FLAGS,checkpoint_key="teacher", use_back_bone_only=True)
-
+    dinov1_model = initDinoV1Model(model_to_load=FLAGS.dino_custom_model_weights,FLAGS=FLAGS,checkpoint_key="teacher")
+    #dinov1_model = vit_small(patch_size=17).to("cuda")
+    #dinov1_model = MLP().to("cuda")
     '''
     data_path = "./data/ePillID_data/classification_data/segmented_nih_pills_224/"
     dinov1_transform = T.Compose([    
@@ -117,12 +121,33 @@ if __name__=="__main__":
 
     print("result:", pred)
     '''
-    
-    ref_data = get_epill_dataloader('refs', FLAGS.batch_size, True)
-    holdout_data = get_epill_dataloader('holdout', FLAGS.batch_size, True)
+
+    dataset = get_epill_dataset('refs', use_epill_transforms=None, use_dinov1_norm=True, crop_transforms=None, do_ocr = False)
+    test_dataset = get_epill_dataset('holdout', use_epill_transforms=None, use_dinov1_norm=True, crop_transforms=None, do_ocr=False) 
+
+    sampler = torch.utils.data.DistributedSampler(dataset, shuffle=False)
+    data_loader_train = torch.utils.data.DataLoader(
+        dataset,
+        sampler=sampler,
+        batch_size=FLAGS.batch_size,
+        num_workers=FLAGS.num_workers,
+        pin_memory=True,
+        drop_last=False,
+    )
+    data_loader_query = torch.utils.data.DataLoader(
+        test_dataset,
+        batch_size=FLAGS.batch_size,
+        num_workers=FLAGS.num_workers,
+        pin_memory=True,
+        drop_last=False,
+    )
+
+    #ref_data = get_epill_dataloader('refs', FLAGS.batch_size, use_epill_transforms=None, use_dinov1_norm=True, crop_transforms=None)
+    #holdout_data = get_epill_dataloader('holdout', FLAGS.batch_size, use_epill_transforms=None, use_dinov1_norm=True, crop_transforms=None)
 
     # extract feature
     print("start extracting feature")
+    '''
     feature_path = "features/"
     ref_features = []
     ref_labels = []
@@ -145,10 +170,10 @@ if __name__=="__main__":
         
         for x in labels:
             ref_labels.append(x)
-     
-    torch.save(ref_features, feature_path+"ref_features_backbone.pt")
+    #torch.save(ref_labels, "ref_labels.pt") 
+    torch.save(ref_features, feature_path+"ref_features_finetune.pt")
     #print("loading ref_features...")
-    #ref_features = torch.load(feature_path+"ref_features.pt")    
+    #ref_features = torch.load(feature_path+"ref_features_backbone.pt.pt")    
     
     holdout_features = []
     holdout_labels = []
@@ -163,7 +188,7 @@ if __name__=="__main__":
         features = features.tolist()
         labels = labels.to("cpu")
         labels = labels.tolist()
-         
+        
         for x in features:
             x = torch.Tensor(x)
             x = F.normalize(x, dim=0)
@@ -171,18 +196,46 @@ if __name__=="__main__":
         
         for x in labels:
             holdout_labels.append(x)
-    
-    torch.save(holdout_features, feature_path+"holdout_features_backbone.pt")
+    #torch.save(holdout_labels, "holdout_labels.pt")
+    torch.save(holdout_features, feature_path+"holdout_features_finetune.pt")
     #print("loading holdout_features...")
-    #holdout_features = torch.load(feature_path+"holdout_features.pt")
+    #holdout_features = torch.load(feature_path+"holdout_features_backbone.pt")
     # calculate cosine similarity
-    
+    '''
+
+    ref_labels = dataset.ids
+    ref_info = dataset.labels
+    #ref_ocr = dataset.ocr_res
+    print("len ref_labels:", len(ref_labels))
+    holdout_labels = test_dataset.ids
+    holdout_info = test_dataset.labels
+    #holdout_ocr = test_dataset.ocr_res
+    print("len holdout_labels:", len(holdout_labels))
+
+    dataset.extract_features(dinov1_model,data_loader=data_loader_train)
+    test_dataset.extract_features(dinov1_model,data_loader=data_loader_query)
+
+    ref_features = []
+    holdout_features = []
+    for i in range(len(dataset)):
+        ref_features.append(dataset.image_features[i])
+    for i in range(len(test_dataset)):
+        holdout_features.append(test_dataset.image_features[i])
+
+    ref_features = torch.from_numpy(np.array(ref_features))
+    holdout_features = torch.from_numpy(np.array(holdout_features))
+
+    ref_features = ref_features.reshape(ref_features.size(0), -1)
+    holdout_features = holdout_features.reshape(holdout_features.size(0), -1) 
+
+    '''
     print("calculate cosine similarity")
     predict_list = []
     
     for i in tqdm(range(len(holdout_features))):
         max_cos=0
         max_label=-1
+        cos_list=[]
         for j in range(len(ref_features)):
             a = holdout_features[i]
             a = a.to("cuda")
@@ -191,31 +244,100 @@ if __name__=="__main__":
             #print("a shape:", a.shape)
             #print("b shape:", b.shape)
             cos = F.cosine_similarity(a, b, dim=0)
-            if cos > max_cos:
-                max_cos = cos
-                max_label = ref_labels[j]
-            #tup = ref_labels[j], cos
-            #cos_list.append(tup)
-        #sorted_cos_list = sorted(cos_list, key=lambda x: x[1], reverse=True)
-        predict_list.append(max_label)
+            #if cos > max_cos:
+            #    max_cos = cos
+            #    max_label = ref_labels[j]
+            tup = ref_labels[j][1], cos
+            cos_list.append(tup)
+        sorted_cos_list = sorted(cos_list, key=lambda x: x[1], reverse=True)
+
+        temp_list=[]
+        for n in range(FLAGS.topK):
+            temp_list.append(sorted_cos_list[n][0])
+        predict_list.append(temp_list)
     torch.save(predict_list, "predict_list_backbone_only.pt")
     print("====predict_list====")
     print("len:", len(predict_list))
     print(predict_list)
     
     #predict_list = torch.load("predict_list_backbone_only.pt")
+    for i in predict_list:
+        print("predict:", i)
     c = 0
     for i in range(len(holdout_labels)):
-        if holdout_labels[i] == predict_list[i]:
+        if holdout_labels[i][1] in predict_list[i]:
             print("match")
             c+=1
     print("c:", c)
-    a_list =[]
-    p_list =[]
+    '''
+    d = ref_features.size(-1)    # dimension
+    nb = ref_features.size(0)    # database size
+    nq = holdout_features.size(0)      # nb of queries
+
+    index = faiss.IndexFlatL2(d)   # build the index
+    print(index.is_trained)
+    index.add(ref_features)    # add vectors to the index
+    print(index.ntotal)
+
+    topK  = FLAGS.topK
+    k = FLAGS.topK                          # we want to see 4 nearest neighbors
+    #D, I = index.search(gallery_features[:5], k) # sanity check
+    #print(I)
+    #print(D)
+    D, I = index.search(holdout_features, k)     # actual search
+
+    a_list = []
+    a_img = []
+    p_list = []
+    p_img = []
+    lcID_list = []
+    pcID_list = []
+    #print("I:", I)
+    #print("len I", len(I))
+    
+    for i in I:
+        temp=[]
+        #temp_lc=[]
+        #temp_pc=[]
+        for idx in i:
+            temp.append(ref_labels[idx])
+            p_img.append(ref_info[idx][7])
+            #temp_lc.append(ref_info[idx][2])
+            #temp_pc.append(ref_info[idx][3])
+        p_list.append(temp)
+        #lcID_list.append(temp_lc)
+        #pcID_list.append(temp_pc)
     for i in holdout_labels:
         a_list.append([i])
-    for i in predict_list:
-        p_list.append([i])
+    #for i in predict_list:
+    #    p_list.append([i])
+    #p_list = predict_list
+    '''
+    merged_list = []
+    for i in range(745):
+        query_lc = lcID_list[i]
+        query_pc = pcID_list[i]
+        match = []
+        unmatch = []
+        for j in range(len(query_lc)):
+            if query_lc[j] == holdout_ocr[i] or query_pc[j] == holdout_ocr[i]:
+                match.append(p_list[i][j])
+            else:
+                unmatch.append(p_list[i][j])
+            merge = match + unmatch
+            merge = merge[:FLAGS.topK]
+        merged_list.append(merge)
+    '''
+    c = 0
+    for i in range(745):
+        print("p_list[i]:", p_list[i], "img:", p_img[i])
+        print("a_list[i]:", a_list[i], "img", holdout_info[i][7])
+        if p_list[i][0] == a_list[i][0]:
+            c+=1
+    print("c:", c)
+    print("merged_list len:", len(p_list))
+    print("a_list len:", len(a_list))
+    
     Map_result = metrics.mapk(a_list, p_list)
     print("MAP score:", Map_result)
     
